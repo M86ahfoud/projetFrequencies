bd22e7cd5ec6a8a578285dee778aa1513dde2346
FIRST\_Mahfoud LAST\_HANI <[MY\_NAME@example.com](mailto:hanimahfoud@gmail.com)>
2024-11-10 17:57:31 +0100

    PageObject

---
+++ pages/checkingFooter.js
@@ -0,0 +1,9 @@
+exports.checkFooter = class checkFooter {
+    constructor(page) {
+        this.page = page
+    }
+
+    async goto() {
+        await this.page.goto('https://www.frequencies.fr/');
+    }
+}
--- tests/checkingFooterSection.spec.js
+++ tests/checkingFooterSection.spec.js
@@ -1,27 +1,54 @@
-import { test, expect, selectors } from "@playwright/test";
-test('Vérification des éléments du bas de page', async ({ page }) => {
-    // Accéder à la page d'acceuil 
-    await page.goto('https://www.frequencies.fr/');
+import { test, expect, chromium } from "@playwright/test";
+import { checkFooter } from "../pages/checkingFooter";
+import selectorFooter from "../fixture/selectorFooter.json"
 
-    // Liste des sélecteurs pour les élements de bas de page
-    const elements = [
-        { label: 'adresse', selector: 'footer >> text=Association Frequencies' },
-        { label: 'Youtube', selector: 'footer a[aria-label="Visitez notre page Youtube"]' },
-        { label: 'Instagram', selector: 'footer a[aria-label="Visitez notre page Instagram"]' },
-        { label: 'Facebook', selector: 'footer a[aria-label="Visitez notre page Facebook"]' },
-        { label: 'Linkedin', selector: 'footer a[aria-label="Visitez notre page Linkedin"]' },
-        { label: 'DeclarationAccessibilité', selector: 'footer >> text=Déclaration d\'accessibilté' },
-        { label: 'Changer le mode', selector: 'footer >> text=Light Mode' },
-        // { label: 'politique de confidentialité', selector: 'footer >> text=Politique de confidentialité' },
-        // { label: 'conditions d\'utilisation', selector: 'footer >> text=Conditions d\'utilisation' },
-        // { label: 'droit d\'auteur', selector: 'footer >> text=Droit d\'auteur' },
-        // { label: 'téléphone', selector: 'footer >> text=Tél' },
-        // { label: 'email', selector: 'footer >> text=Email' },
-    ];
-    // Vérification de la présence et visibilité de chaque élément
-    for (const { label, selector } of elements) {
-        const element = page.locator(selector);
-        await expect(element, `L'élément ${label} devrait être présent et visible`).toBeVisible();
-    }
+test.describe('Vérification des éléments du bas de page', async () => {
+
+    let browser;
+    let context;
+    let page;
+    let checkFooterInstance;
+
+    test.beforeAll(async() => {
+        browser = await chromium.launch();
+    });
+
+    test.beforeEach(async() => {
+        context = await browser.newContext();
+        page = await context.newPage();
+        checkFooterInstance = new checkFooter(page)
+    });
+
+    test.afterAll(async() => {
+        await browser.close()
+    });
+
+    test('Vérification des éléments du bas de page', async ()=> {
+        
+        // Accéder à la page d'acceuil 
+       await checkFooterInstance.goto();
+    
+        // Liste des sélecteurs pour les élements de bas de page
+        const elements = [
+            { label: 'adresse', selector: selectorFooter.selectoreAdresse },
+            { label: 'Youtube', selector: selectorFooter.selectorYoutube },
+            { label: 'Instagram', selector: selectorFooter.selectorInstagram },
+            { label: 'Facebook', selector: selectorFooter.selectorFacebook },
+            { label: 'Linkedin', selector: selectorFooter.selectorLinkedin },
+            { label: 'DeclarationAccessibilité', selector: selectorFooter.selectorAccessibilité },
+            { label: 'Changer le mode', selector: selectorFooter.selectorChangeMode },
+            // { label: 'politique de confidentialité', selector: 'footer >> text=Politique de confidentialité' },
+            // { label: 'conditions d\'utilisation', selector: 'footer >> text=Conditions d\'utilisation' },
+            // { label: 'droit d\'auteur', selector: 'footer >> text=Droit d\'auteur' },
+            // { label: 'téléphone', selector: 'footer >> text=Tél' },
+            // { label: 'email', selector: 'footer >> text=Email' },
+        ];
+        // Vérification de la présence et visibilité de chaque élément
+        for (const { label, selector } of elements) {
+            const element = page.locator(selector);
+            await expect(element, `L'élément ${label} devrait être présent et visible`).toBeVisible();
+        }
+
+    })
 
 })
