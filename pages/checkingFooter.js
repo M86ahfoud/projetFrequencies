9d8e466de56b49de2fe7646d95a06f00295eee67
FIRST\_Mahfoud LAST\_HANI <[MY\_NAME@example.com](mailto:hanimahfoud@gmail.com)>
2024-11-20 18:28:55 +0100

    footer check

--- pages/checkingFooter.js
+++ pages/checkingFooter.js
@@ -1,3 +1,4 @@
+
 exports.checkFooter = class checkFooter {
     constructor(page) {
         this.page = page
--- tests/checkingFooterSection.spec.js
+++ tests/checkingFooterSection.spec.js
@@ -1,34 +1,28 @@
 import { test, expect, chromium } from "@playwright/test";
 import { checkFooter } from "../pages/checkingFooter";
-import selectorFooter from "../fixture/selectorFooter.json"
+import selectorFooter from "../fixture/selectorFooter.json";
+import {setupHooks, getPage, getContext} from "../pages/hooks"
 
+setupHooks();
 test.describe('Vérification des éléments du bas de page', async () => {
 
-    let browser;
-    let context;
+    let checkFooterInstance; 
     let page;
-    let checkFooterInstance;
-
-    test.beforeAll(async() => {
-        browser = await chromium.launch();
-    });
-
-    test.beforeEach(async() => {
-        context = await browser.newContext();
-        page = await context.newPage();
-        checkFooterInstance = new checkFooter(page)
-    });
-
-    test.afterAll(async() => {
-        await browser.close()
-    });
-
-    test('Vérification des éléments du bas de page', async ()=> {
-        
-        // Accéder à la page d'acceuil 
-       await checkFooterInstance.goto();
+    let context
     
-        // Liste des sélecteurs pour les élements de bas de page
+    test.beforeEach(async ()=> {
+        page = getPage(); 
+        context = getContext();
+        checkFooterInstance = new checkFooter(page);
+        // Access the homepage 
+        await checkFooterInstance.goto();
+    })
+
+    test('Vérification des éléments du bas de page', async () => {
+
+        
+
+        // List of selectors for footer elements
         const elements = [
             { label: 'adresse', selector: selectorFooter.selectoreAdresse },
             { label: 'Youtube', selector: selectorFooter.selectorYoutube },
@@ -43,12 +37,74 @@ test.describe('Vérification des éléments du bas de page', async () => {
             // { label: 'téléphone', selector: 'footer >> text=Tél' },
             // { label: 'email', selector: 'footer >> text=Email' },
         ];
-        // Vérification de la présence et visibilité de chaque élément
+        // Verification of the presence and visibility of each element
         for (const { label, selector } of elements) {
             const element = page.locator(selector);
             await expect(element, `L'élément ${label} devrait être présent et visible`).toBeVisible();
         }
 
+    });
+
+    test("Verification of the Footer link and button", async () => {
+
+        // Access the homepage  
+        // await checkFooterInstance.goto();
+
+        // Store all buttons and links in array
+        const buttons = [
+            { selector: 'Visitez notre page Youtube', expectedUrl: 'https://consent.youtube.com/m?continue=https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCgxfEuByVQ8X2k5Tk5yVwhg%3Fcbrd%3D1&gl=FR&m=0&pc=yt&cm=2&hl=en&src=1' },
+            { selector: 'Visitez notre page Instagram', expectedUrl: 'https://www.instagram.com/frequencies.production/' },
+            { selector: 'Visitez notre page Facebook', expectedUrl: 'https://www.facebook.com/frequencies.production' },
+            { selector: 'Visitez notre page Linkedin', expectedUrl: 'https://www.linkedin.com/company/frequencies' }
+        ];
+
+        // Verify each redirect button or link
+        for (const buttonInfo of buttons) {
+
+            const button = await page.getByLabel(buttonInfo.selector);
+
+            console.log(`test en cours pour le bouton: ${buttonInfo.selector}`);
+
+            // Monitor the opening of a new tab
+            const [newTab] = await Promise.all([
+                context.waitForEvent('page'),
+                button.click(),
+            ]);
+
+            // Wait for the new tab to load
+            await newTab.waitForLoadState();
+
+            // Check the URL of the opened tab
+            const currentUrl = newTab.url();
+            if (currentUrl === buttonInfo.expectedUrl) {
+                console.log(`Test réussi pour${buttonInfo.selector} : le bouton redirige vers ${currentUrl}`);
+            } else {
+                console.log(`Test échoué pour ${buttonInfo.selector} : URL inattendue - ${currentUrl}`);
+            }
+
+            // Close the tab
+            await newTab.close();
+
+        }
+
+
+        // Click on the link of accessibilité  
+        await page.getByRole('link', { name: 'Déclaration d\'accessibilté' }).click();
+
+        // Verify that link redirect to the correct page
+        await expect(page).toHaveURL('https://www.frequencies.fr/accessibilite');
+
+        // Identify the element affected by the mode change
+        const header = page.locator('header'); 
+
+        // Verify the initial mode (light mode)
+        await expect (header).toHaveClass(/light-menu/);
+
+        // Click on the button to change the mode
+        await page.getByLabel('\'Changer le thème du site :').click();
+
+        // Verify the dark mode after pressing the button. 
+        await expect (header).toHaveClass(/dark-menu/);
     })
 
 })
